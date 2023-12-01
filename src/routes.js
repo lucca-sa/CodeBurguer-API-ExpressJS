1cd93e59bf9ff0b0c047b8540108589364dde12d
lucca-sa <scherer.lucca@gmail.com>
2023-11-30 22:11:44 -0300

    db connection created

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from 'express'
 
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
+
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'admin',
+    email: 'admin@email.com',
+    password_hash: 'admin',
+    admin: true,
+  })
+
+  return response.json(user)
 })
 
 export default routes
