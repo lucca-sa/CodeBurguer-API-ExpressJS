2bd009ec6a235ec5279c17992476951b8b644dd4
lucca-sa <scherer.lucca@gmail.com>
2023-12-10 23:44:26 -0300

    updating category and product

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -22,6 +22,7 @@ class CategoryController {
       }
 
       const { name } = request.body
+      const { filename: path } = request.file
 
       const categoryExists = await Category.findOne({
         where: {
@@ -33,7 +34,7 @@ class CategoryController {
         return response.status(400).json({ error: 'Category already exists' })
       }
 
-      const { id } = await Category.create({ name })
+      const { id } = await Category.create({ name, path })
 
       return response.json({ id, name })
     } catch (err) {
@@ -45,6 +46,47 @@ class CategoryController {
     const category = await Category.findAll()
 
     return response.json(category)
+  }
+
+  async update(request, response) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string(),
+      })
+
+      try {
+        await schema.validateSync(request.body, { abortEarly: false })
+      } catch (err) {
+        return response.status(400).json({ error: err.errors })
+      }
+
+      const { admin: isAdmin } = await User.findByPk(request.userId)
+
+      if (!isAdmin) {
+        return response.status(401).json()
+      }
+
+      const { name } = request.body
+
+      const { id } = request.params
+
+      const category = await Category.findByPk(id)
+
+      if (!category) {
+        return response.status(400).json({ error: 'Category not found' })
+      }
+
+      let path
+      if (request.file) {
+        path = request.file.filename
+      }
+
+      await Category.update({ name, path }, { where: { id } })
+
+      return response.status(200).json()
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
