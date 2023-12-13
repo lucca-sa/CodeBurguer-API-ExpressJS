9b1fdd291080d71eb850e2addd4385168ee9de89
lucca-sa <scherer.lucca@gmail.com>
2023-12-12 21:53:09 -0300

    cors ;P

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import routes from './routes'
 import { resolve } from 'path'
 
 import './database'
+import cors from 'cors'
 
 class App {
   constructor() {
     this.app = express()
+    this.app.use(cors())
 
     this.middlewares()
     this.routes()
